5e16dfe3f92785c487b9196ab1c0611616c00f1a
arsh23 <programmer.arsh@gmail.com>
2016-12-17 18:23:05 +0530

    added regex

--- scraper.py
+++ scraper.py
@@ -4,16 +4,19 @@ import asyncio
 import aiohttp
 from lxml import html
 
+DOMAIN = 'https://en.wikipedia.org'
+REGEX = re.compile(r"^(\/wiki\/[^:#\s]+)(?:$|#)")
+
 async def get(url):
     r = await aiohttp.request('GET', url)
     html_code = await r.read()
-    html_code = html_code.decode("utf-8")
-    return html_code
+    return html_code.decode("utf-8")
 
 
 def extract_urls(html_code):
     tree = html.fromstring(html_code)
-    urls = tree.xpath('//a/@href')
+    urls_list = map(REGEX.findall, tree.xpath('//a/@href'))
+    urls = {DOMAIN + x[0] for x in urls_list if x != []}
     return urls
 
 
@@ -21,7 +24,6 @@ async def test():
     url = 'https://en.wikipedia.org/wiki/Python_(programming_language)'
     print('Request sent for {}'.format(url))
     html_code = await get(url)
-    print(type(html_code))
     print('html recieved')
     urls = extract_urls(html_code)
     return urls
@@ -31,9 +33,9 @@ def main():
     loop = asyncio.get_event_loop()
     coroutine = test()
     res = loop.run_until_complete(coroutine)
-    print(res)
-    # urls = loop.run_until_complete(coroutine)
-    # print(urls)
+    for u in res:
+        print(u)
+    print(len(u))
 
 if __name__ == '__main__':
     main()
