ab83384bb54da50f26a1c07fb6b7e7ce16f19856
arsh23 <programmer.arsh@gmail.com>
2016-12-18 05:33:07 +0530

    added error handling and caching

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import re
 import asyncio
 import async_timeout
+from timeit import default_timer as timer
 
 import aiohttp
 from lxml import html
@@ -9,7 +10,11 @@ DOMAIN = 'https://en.wikipedia.org'
 REGEX = re.compile(r"^(\/wiki\/[^:#\s]+)(?:$|#)")
 Q = asyncio.Queue()
 MAX_DEPTH = 1
+MAX_RETRIES = 5
 MAX_WORKERS = 20
+
+count = 0
+cache = set()
 
 async def get(session, url, timeout=10):
     with async_timeout.timeout(timeout):
@@ -24,26 +29,39 @@ def extract_urls(html_code):
 
 
 async def worker(loop):
+    global count
     async with aiohttp.ClientSession(loop=loop) as session:
         while True:
-            depth, url = await Q.get()
+            depth, url, retries = await Q.get()
             if url == None:
                 break
+            if url in cache:
+                continue
 
-            print('Request sent for {}'.format(url))
-            urls = extract_urls(await get(session, url))
-            print('Done : {}'.format(url))
+            try:
+                html_code = await get(session, url)
+            except asyncio.TimeoutError:
+                if retries + 1 <= MAX_RETRIES:
+                    # print('Timeout on {} retrying...'.format(url))
+                    Q.put_nowait((depth, url, retries + 1))
+                    continue
 
-            if depth + 1 > MAX_DEPTH:
-                for _ in range(MAX_WORKERS):
-                    Q.put_nowait((None, None))
-            else:
+            # print('Request sent for {}'.format(url))
+            urls = extract_urls(html_code)
+            count += 1
+            cache.add(url)
+            # print('Done : {}'.format(url))
+
+            if depth + 1 <= MAX_DEPTH:
                 for url in urls:
-                    Q.put_nowait((depth + 1, url))
+                    Q.put_nowait((depth + 1, url, retries))
+            elif depth + 1 > MAX_DEPTH and Q.qsize() == 1:
+                for _ in range(MAX_WORKERS):
+                    Q.put_nowait((None, None, None))
 
 
 def main():
-    Q.put_nowait((0, DOMAIN + '/wiki/Python_(programming_language)'))
+    Q.put_nowait((0, DOMAIN + '/wiki/Python_(programming_language)', 0))
     loop = asyncio.get_event_loop()
     workers = [worker(loop) for x in range(MAX_WORKERS)]
     loop.run_until_complete(asyncio.wait(workers))
@@ -51,4 +69,7 @@ def main():
 
 
 if __name__ == '__main__':
+    start = timer()
     main()
+    end = timer()
+    print('Time Taken for {} requests : {} sec'.format(count, end - start))
