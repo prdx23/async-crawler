dfdd8c5629a7161427c02f852f9cbd880804d4f4
arsh23 <programmer.arsh@gmail.com>
2016-12-20 03:37:13 +0530

    added args parsing

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import re
 import json
 import asyncio
+import argparse
 import async_timeout
 from timeit import default_timer as timer
 
@@ -52,7 +53,7 @@ async def worker(loop):
             count += 1
             cache.add(url)
             graph[url.split('wiki/')[1]] = [x.split('wiki/')[1] for x in urls]
-            print('Done : {}'.format(url))
+            print('Crawled : {}'.format(url))
 
             if depth + 1 <= MAX_DEPTH:
                 for url in urls:
@@ -62,19 +63,45 @@ async def worker(loop):
                     Q.put_nowait((None, None, None))
 
 
-def main():
-    Q.put_nowait((0, DOMAIN + '/wiki/Python_(programming_language)', 0))
+def main(start_url):
+    Q.put_nowait((0, DOMAIN + start_url, 0))
     loop = asyncio.get_event_loop()
     workers = [worker(loop) for x in range(MAX_WORKERS)]
     loop.run_until_complete(asyncio.wait(workers))
     loop.close()
+    output_json()
 
+
+def output_json():
     with open('graph.json', 'w') as fp:
         json.dump(graph, fp)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='A recursive wikipidia crawler which creates a graph of '
+        'connected pages')
+    parser.add_argument(
+        '-d', '--depth', type=int,
+        help='Max recursion depth (default=1)', default=None)
+    parser.add_argument(
+        '-w', '--workers', type=int,
+        help='Max concurrent Workers (default=1)', default=None)
+    parser.add_argument(
+        '-r', '--retries', type=int,
+        help='Max no of retries for timeout errors (default=1)', default=None)
+    parser.add_argument(
+        '-u', '--url', type=str,
+        help='Starting url', default=None)
+
+    args = parser.parse_args()
+    MAX_DEPTH = args.depth if args.depth else MAX_DEPTH
+    MAX_WORKERS = args.workers if args.workers else MAX_WORKERS
+    MAX_RETRIES = args.retries if args.retries else MAX_RETRIES
+    start_url = args.url if args.url else '/wiki/Python_(programming_language)'
+
+    print('Started crawling...')
     start = timer()
-    main()
+    main(start_url)
     end = timer()
     print('Time Taken for {} requests : {} sec'.format(count, end - start))
